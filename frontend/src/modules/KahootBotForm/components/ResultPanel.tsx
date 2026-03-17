28faf7e14bacaa64c8b0a363694a10cd50f3c4bd
Daniil Ignatjev <daniil.ignatjev@gmail.com>
2026-03-17 16:06:42 +0200

    Added ad (direct link) and small fixes

--- frontend/src/modules/KahootBotForm/components/ResultPanel.tsx
+++ frontend/src/modules/KahootBotForm/components/ResultPanel.tsx
@@ -3,7 +3,7 @@ import getText from '../../../shared/texts/texts';
 function ResultPanel(props: any) {
 	return (
 		<>
-			<div className="flex justify-center margin-top">
+			<div className="flex justify-center margin-top" id="dataFieldset">
 				<fieldset className="fieldset bg-base-100 border-base-300 rounded-box w-xs border p-4 mr-[10%] ml-[10%] small-font">
 					<legend className="fieldset-legend">{getText('result.legend')}</legend>
 

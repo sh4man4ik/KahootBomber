c37ba4241ba20f190fcded3e5e791c9b41124227
Daniil Ignatjev <daniil.ignatjev@gmail.com>
2026-04-09 17:46:39 +0300

    Added donate button

--- frontend/src/modules/Footer/Footer.tsx
+++ frontend/src/modules/Footer/Footer.tsx
@@ -1,4 +1,5 @@
 import getText from '../../shared/texts/texts';
+import DonateButton from './components/DonateButton';
 
 function Footer() {
 	return (
@@ -6,6 +7,7 @@ function Footer() {
 			<footer className="border-t border-base-300 margin-top footer sm:footer-horizontal footer-center main-content-color p-4 small-font">
 				<aside>
 					<p>{getText('footer.text')}</p>
+					<DonateButton></DonateButton>
 				</aside>
 			</footer>
 		</>
